1107be2e4c6ce02c98301cd379c13ba1ea78f0ad
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-03 23:02:40 +0000

    Remove tests (for now?)

--- tests/helpers.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-
-from ibdatastream.ib import Contract, IBClient
-
-
-class StubIBClient(IBClient):
-    async def qualify_contract_inplace(self, contract: Contract) -> None:
-        contract.conId = 1234
