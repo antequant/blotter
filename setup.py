a2dc4d6a5afc8ac66cdbf6f5944753638da534ba
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-03 22:16:44 +0000

    Install all the things!

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="ibdatastream",
+    name="market-blotter",
     version="0.1.0",
     author="Justin Spahr-Summers",
     author_email="justin@jspahrsummers.com",
-    description="Microservice to connect to Interactive Brokers and stream market data elsewhere",
+    description="Microservice to connect to Interactive Brokers and stream market data into Google BigQuery",
     long_description=long_description,
     long_description_content_type="text/markdown",
     license="MIT",
-    url="https://github.com/jspahrsummers/ibdatastream",
+    url="https://github.com/jspahrsummers/blotter",
     packages=find_packages(),
-    package_data={"ibdatastream": ["ibdatastream.proto", "py.typed"]},
+    package_data={"blotter": ["blotter.proto", "py.typed"]},
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Console",
@@ -28,7 +28,12 @@ setup(
         "Topic :: Internet",
         "Topic :: Office/Business :: Financial :: Investment",
     ],
-    install_requires=["grpcio ~= 1.23", "ib-insync ~= 0.9.56", "protobuf ~= 3.9"],
+    install_requires=[
+        "grpcio ~= 1.25",
+        "ib-insync ~= 0.9.58",
+        "google-cloud-bigquery ~= 1.22",
+        "pandas ~= 0.25.3",
+    ],
     keywords="trading investing finance ib ibkr tws",
-    entry_points={"console_scripts": ["ibdatastream = ibdatastream.__main__:main"]},
+    entry_points={"console_scripts": ["blotter = blotter.__main__:main"]},
 )
