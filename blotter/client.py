c6f93d0466f4e99705c179ebf117accaa464e37d
Justin Spahr-Summers <justin@jspahrsummers.com>
2019-12-04 22:50:41 +0000

    Delete client.py

--- blotter/client.py
+++
@@ -1,8 +0,0 @@
-from . import blotter_pb2_grpc
-
-import grpc
-
-
-def new_test_client(port: int, host: str = "localhost") -> blotter_pb2_grpc.BlotterStub:
-    channel = grpc.insecure_channel(f"{host}:{port}")
-    return blotter_pb2_grpc.BlotterStub(channel)
